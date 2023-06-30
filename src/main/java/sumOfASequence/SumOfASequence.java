2bbbcd44a33f2a01a44c506845644df47f00a048
Christian Hajek <chris@devseal.eu>
2023-06-30 12:06:13 +0200

    Fixed Bugs & removed 1 not (anymore) working Kata

--- src/main/java/sumOfASequence/SumOfASequence.java
+++ src/main/java/sumOfASequence/SumOfASequence.java
@@ -4,7 +4,7 @@ public class SumOfASequence {
     public int sequenceSum(int start, int end, int step) {
         int sum = 0;
         int current = start;
-        while (current <= end) {
+        while (current <= end && step != 0) {
             sum += current;
             current += step;
         }
@@ -14,6 +14,7 @@ public class SumOfASequence {
     public int sequenceSumV2(int start, int end, int step) {
         int sum = 0;
         for (int current = start; current <= end; current += step) {
+            if (step == 0) break;
             sum += current;
         }
         return sum;
